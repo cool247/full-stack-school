93ae5000bd848c26ede27bdc2731dd308f0eaf7a
Sadanand Gopal <sadanandgpl@gmail.com>
2024-09-24 20:19:16 +0530

    dav

--- src/app/[[...sign-in]]/page.tsx
+++ src/app/[[...sign-in]]/page.tsx
@@ -19,11 +19,11 @@ const LoginPage = () => {
   }, [user, router]);
 
   return (
-    <div className="h-screen flex items-center justify-center bg-lamaSkyLight">
+    <div className="h-screen flex items-center justify-center bg-davSkyLight">
       
           <h1 className="text-xl font-bold flex items-center gap-2">
             <Image src="/logo.png" alt="" width={24} height={24} />
-            SchooLama
+            Schoodav
           </h1>
           <h2 className="text-gray-400">Sign in to your account</h2>
           
