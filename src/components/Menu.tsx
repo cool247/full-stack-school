ff57bb5659faa28073ff54e281e4e00d9d0108b6
Sadanand Gopal <sadanandgpl@gmail.com>
2024-09-25 01:27:20 +0530

    Update Menu.tsx

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -129,7 +129,7 @@ const Menu = async () => {
             if (item.visible.includes(role || "")) {
               return (
                 <Link
-                  href={item.href === "/dashboard" ? role || "/" : item.href}
+                  href={item.href === "/dashboard" ? '/'+role || "/" : item.href}
                   key={item.label}
                   // onClick={handleSignOut}
                   className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-davSkyLight">
