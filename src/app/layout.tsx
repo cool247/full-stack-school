93a9b37942ed01963a6727cf919cf2cea59be16c
Sadanand Gopal <sadanandgpl@gmail.com>
2024-09-25 01:11:40 +0530

    anaytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
         <ToastContainer position="bottom-right" theme="dark" />
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
